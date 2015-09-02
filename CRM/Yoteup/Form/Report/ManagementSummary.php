16f615cfed019b5a317065eb65449593076764f4
Edsel <edsel.lopez@jmaconsulting.biz>
2015-09-02 18:33:08 +0530

    NRM-4 Added condition for valid webforms

--- CRM/Yoteup/Form/Report/ManagementSummary.php
+++ CRM/Yoteup/Form/Report/ManagementSummary.php
@@ -46,6 +46,7 @@ class CRM_Yoteup_Form_Report_ManagementSummary extends CRM_Report_Form {
 
   function select() {
     $select = $this->_columnHeaders = array();
+    $urlWhere = self::createURLCondition();
 
     $this->_columnHeaders["description"]['title'] = " ";
     $this->_columnHeaders["perday_visitor_count"]['title'] = " ";
@@ -55,7 +56,11 @@ class CRM_Yoteup_Form_Report_ManagementSummary extends CRM_Report_Form {
        UNION 
        SELECT 'Total unique new visitors for all time' as description, SUM(perday_visitor) as perday_visitor_count FROM
        ( SELECT COUNT(DISTINCT((SUBSTRING_INDEX(SUBSTRING_INDEX(location, '://', -1), '/', 1)))) as perday_visitor  
-       FROM  {$this->_drupalDatabase}.watchdog) as y ";
+       FROM  {$this->_drupalDatabase}.watchdog) as y 
+       UNION
+       SELECT 'Total starts for individual forms and surveys for the day' as description, SUM(perday_start) as perday_visitor_count FROM
+       ( SELECT COUNT(DISTINCT(location)) as perday_start
+       FROM  {$this->_drupalDatabase}.watchdog WHERE DATE(FROM_UNIXTIME(timestamp)) = DATE(NOW()) {$urlWhere}) as z";
   }
 
   function from() {
@@ -96,8 +101,9 @@ class CRM_Yoteup_Form_Report_ManagementSummary extends CRM_Report_Form {
   function getWebforms() {
     $this->webForms = array();
 
-    $sql = "SELECT w.nid, n.title FROM {$this->_drupalDatabase}.webform w 
-      INNER join {$this->_drupalDatabase}.node n ON n.nid = w.nid";
+    $sql = "SELECT w.nid, n.title
+      FROM {$this->_drupalDatabase}.webform w 
+      INNER JOIN {$this->_drupalDatabase}.node n ON n.nid = w.nid";
     $dao = CRM_Core_DAO::executeQuery($sql);
     while ($dao->fetch()) {
       $this->webForms[$dao->nid] = $dao->title;
@@ -105,16 +111,40 @@ class CRM_Yoteup_Form_Report_ManagementSummary extends CRM_Report_Form {
   }
   
   function getDefaultWebforms() {
-    $defaults = array();
+    $defaults = $urls = $this->urls = array();
     
-    $sql = "SELECT w.nid, n.title FROM {$this->_drupalDatabase}.webform w 
-      INNER join {$this->_drupalDatabase}.node n ON n.nid = w.nid
+    $sql = "SELECT w.nid, u.alias
+      FROM {$this->_drupalDatabase}.webform w
+      INNER JOIN {$this->_drupalDatabase}.node n ON n.nid = w.nid
+      INNER JOIN {$this->_drupalDatabase}.url_alias u ON u.source = CONCAT_WS('/', 'node', w.nid)
       WHERE w.nid NOT IN (131, 132, 103, 198, 71, 75, 190, 97, 199)";
     $dao = CRM_Core_DAO::executeQuery($sql);
     while ($dao->fetch()) {
       $default[] = $dao->nid;
+      $urls[$dao->nid] = $dao->alias;
     }
+    $this->urls = $urls;
     return $default;
+  }
+  
+  function createURLCondition() {
+    // First get submitted params from webform
+    $webformOP = $this->_params['webforms_op'];
+    $webformParams = $this->_params['webforms_value'];
+    $urls = $this->urls;
+    // Compute the intersection
+    if ($webformOP == 'in') {
+      $diff = array_flip(array_intersect(array_flip($urls), $webformParams));
+    }
+    else if ($webformOP == 'notin') {
+      $diff = array_flip(array_diff(array_flip($urls), $webformParams));
+    }
+    if (empty($diff)) {
+      return FALSE;
+    } 
+    $statement = implode("' OR location LIKE '%", $diff);
+    $sql = " AND location LIKE '%{$statement}'";
+    return $sql;
   }
 
   function alterDisplay(&$rows) {
