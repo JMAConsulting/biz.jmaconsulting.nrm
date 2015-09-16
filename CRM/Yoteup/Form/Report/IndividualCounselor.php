5219454227aca467b410474af10ffacb47125295
Edsel <edsel.lopez@jmaconsulting.biz>
2015-09-16 18:25:09 +0530

    NRM-3 Bug fixes

--- CRM/Yoteup/Form/Report/IndividualCounselor.php
+++ CRM/Yoteup/Form/Report/IndividualCounselor.php
@@ -501,14 +501,14 @@ class CRM_Yoteup_Form_Report_IndividualCounselor extends CRM_Report_Form {
       }
       if (CRM_Utils_Array::value('civicrm_contact_info_request', $row)) {
         $rows[$rowNum]['civicrm_contact_info_request'] = self::getCustomFieldDataLables($row['civicrm_contact_info_request']);
-        $purl = CRM_Core_DAO::singleValueQuery("SELECT CONCAT(\"'%\", purl_145 ,\"%'\") FROM civicrm_value_nrmpurls_5 WHERE entity_id = {$row['civicrm_contact_contact_id']}");
+        $purl = CRM_Core_DAO::singleValueQuery("SELECT CONCAT(\"'%.\", purl_145 ,\".%'\") FROM civicrm_value_nrmpurls_5 WHERE entity_id = {$row['civicrm_contact_contact_id']}");
         if ($purl) {
           $sql = "SELECT location FROM {$this->_drupalDatabase}.watchdog
           WHERE location REGEXP '[[:alnum:]]+.pdf'
           AND location LIKE {$purl}";
           $dao = CRM_Core_DAO::executeQuery($sql);
           if ($dao->N) {
-            $string = "<hr><b>Downloads:</b><br/>";
+            $string = "<br/><hr><b>Downloads:</b><br/>";
           }
           while ($dao->fetch()) {
             $string .= urldecode(basename($dao->location)) . "<br/>";
