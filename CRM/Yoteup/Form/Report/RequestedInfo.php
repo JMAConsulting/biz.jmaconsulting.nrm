6270525c702146e4371dd4cc5b3a630fcad6f5ea
Pradeep Nayak <pradpnayak@gmail.com>
2015-10-27 19:37:27 +0530

    --NRM-25, include temp table in where clause

--- CRM/Yoteup/Form/Report/RequestedInfo.php
+++ CRM/Yoteup/Form/Report/RequestedInfo.php
@@ -124,7 +124,7 @@ class CRM_Yoteup_Form_Report_RequestedInfo extends CRM_Report_Form {
   }
 
   function from() { 
-    CRM_Yoteup_BAO_Yoteup::reportFromClause($this->_from);
+    CRM_Yoteup_BAO_Yoteup::reportFromClause($this->_from, TRUE);
   }
 
   function where() {
