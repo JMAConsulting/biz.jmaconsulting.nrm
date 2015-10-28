2b11f95325f04c019003a32d2c8f3e980292e0a4
Edsel <edsel.lopez@jmaconsulting.biz>
2015-10-28 17:05:46 +0530

    NRM-22 Added VIP Application Report for Chowan

---
+++ CRM/Yoteup/Form/Report/VipApplication.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Yoteup_Form_Report_VipApplication',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'VIP Application Report',
+      'description' => 'VIP Application Report (biz.jmaconsulting.yoteup)',
+      'class_name' => 'CRM_Yoteup_Form_Report_VipApplication',
+      'report_url' => 'chowan/vipapplication',
+      'component' => '',
+    ),
+  ),
+);
---
+++ CRM/Yoteup/Form/Report/VipApplication.php
@@ -0,0 +1,81 @@
+<?php
+
+class CRM_Yoteup_Form_Report_VipApplication extends CRM_Report_Form {
+
+  protected $_summary = NULL;
+
+  protected $_customGroupGroupBy = FALSE; 
+
+  function __construct() { 
+    $this->_columns = array(
+      'civicrm_contact' => array(
+        'dao' => 'CRM_Contact_DAO_Contact',
+        'fields' => array(
+          'display_name' => array(
+            'title' => ts('Student Info'),
+            'required' => TRUE,
+            'default' => TRUE,
+            'no_repeat' => TRUE,
+          ),
+        ),
+        'grouping' => 'contact-fields',
+      ),
+    );
+    $this->_groupFilter = FALSE;
+    $this->_tagFilter = FALSE;
+    parent::__construct();
+  }
+
+  function preProcess() {
+    $this->assign('reportTitle', ts('VIP Application Report'));
+    parent::preProcess();
+  }
+
+  function select() {
+    $this->_select = "SELECT GROUP_CONCAT(CONCAT( wc.name, ': ', wsd.data), '<br/>' ) as applications";
+    $this->_columnHeaders['applications']['title'] = ts('Applications');
+  }
+
+  function from() {
+    $config = CRM_Core_Config::singleton();
+    $dsnArray = DB::parseDSN($config->userFrameworkDSN);
+    $drupalDb = $dsnArray['database'];
+    $this->_from = "FROM {$drupalDb}.webform_submitted_data wsd 
+      LEFT JOIN civicrm_contact contact_civireport  ON wsd.data = contact_civireport.id AND wsd.cid = 2
+      LEFT JOIN {$drupalDb}.webform_component wc ON wc.cid = wsd.cid
+      LEFT JOIN {$drupalDb}.webform_submissions ws ON ws.sid = wsd.sid";
+  }
+
+  function where() {
+    CRM_Yoteup_BAO_Yoteup::reportWhereClause($this->_where, 70);
+  }
+
+  function groupBy() {
+    $this->_groupBy = "GROUP BY wsd.sid";
+  }
+
+  function orderBy() {
+    return FALSE;
+  }
+
+  function postProcess() {
+
+    $this->beginPostProcess();
+
+    $sql = $this->buildQuery(FALSE);
+
+    $rows = array();
+    $this->buildRows($sql, $rows);
+
+    $this->formatDisplay($rows);
+    $this->doTemplateAssignment($rows);
+    $this->endPostProcess($rows);
+  }
+
+  function alterDisplay(&$rows) {
+    // custom code to alter rows
+    foreach ($rows as $rowNum => &$row) {
+      $row = str_replace('<br/>,', '<br/>', $row);
+    }
+  }
+}
