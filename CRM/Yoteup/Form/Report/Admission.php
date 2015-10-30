7848c8d6968fbbfd54286f47f97612e0d77b3cfe
Pradeep Nayak <pradpnayak@gmail.com>
2015-10-30 17:17:32 +0530

    --NRM-19, changed alias name

--- CRM/Yoteup/Form/Report/Admission.php
+++ CRM/Yoteup/Form/Report/Admission.php
@@ -44,14 +44,20 @@ class CRM_Yoteup_Form_Report_Admission extends CRM_Report_Form {
       'Application_Type' => array(
         'title' => 'Application Type',
       ),
-      'First_Name' => array(
+      'First_Name_1' => array(
         'title' => 'First Name',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 156,
       ),
       'Middle_Name' => array(
         'title' => 'Middle Name',
       ),
-      'Last_Name' => array(
+      'Last_Name_1' => array(
         'title' => 'Last Name',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 159,
       ),
       'Name_Suffix' => array(
         'title' => 'Name Suffix',
@@ -63,7 +69,7 @@ class CRM_Yoteup_Form_Report_Admission extends CRM_Report_Form {
         'title' => 'Preferred/Nickname',
       ),
       'Gender' => array(
-                        'title' => 'Gender',
+        'title' => 'Gender',
         'columnName' => 'g.label',
       ),
       'Birth_Date' => array(
@@ -93,244 +99,343 @@ class CRM_Yoteup_Form_Report_Admission extends CRM_Report_Form {
       'Home_Phone' => array(
         'title' => 'Home Phone',
       ),
-      'Mobile Phone' => array(
+      'Mobile_Phone' => array(
         'title' => 'Mobile Phone',
       ),
-      'Email' => array(
+      'Email_1' => array(
         'title' => 'Email',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 155,
       ),
-      'Primary Academic Interest' => array(
+      'Primary_Academic_Interest' => array(
         'title' => 'Primary Academic Interest',
       ),
-      'Do you plan to play an intercollegiate sport at Brevard?' => array(
+      'Do_you_plan_to_play_an_intercollegiate_sport_at_Brevard' => array(
         'title' => 'Do you plan to play an intercollegiate sport at Brevard?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'If yes, what sport?' => array(
+      'If_yes_what_sport' => array(
         'title' => 'If yes, what sport?',
       ),
-      'Outdoor Interests' => array(
+      'Outdoor_Interests' => array(
         'title' => 'Outdoor Interests',
       ),
-      'Are you being recruited by a Brevard College coach?' => array(
+      'Are_you_being_recruited_by_a_Brevard_College_coach' => array(
         'title' => 'Are you being recruited by a Brevard College coach?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'Performance Instrument' => array(
+      'Performance_Instrument' => array(
         'title' => 'Performance Instrument',
       ),
-      'Voice Type' => array(
+      'Voice_Type' => array(
         'title' => 'Voice Type',
       ),
-      'Are you a U.S. Citizen?' => array(
+      'Are_you_a_U_S_Citizen' => array(
         'title' => 'Are you a U.S. Citizen?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'If not a U.S. Citizen, of what country are you officially a citizen?' => array(
+      'If_not_a_U_S_Citizen_of_what_country_are_you_officially_a_citizen' => array(
         'title' => 'If not a U.S. Citizen, of what country are you officially a citizen?',
       ),
-      'Country of Birth' => array(
+      'Country_of_Birth' => array(
         'title' => 'Country of Birth',
       ),
-      'Current Visa Status in the U.S.' => array(
+      'Current_Visa_Status_in_the_U_S' => array(
         'title' => 'Current Visa Status in the U.S.',
       ),
-      'Number of years residing in the USA' => array(
+      'Number_of_years_residing_in_the_USA' => array(
         'title' => 'Number of years residing in the USA',
       ),
-      'Are you a Veteran?' => array(
+      'Are_you_a_Veteran' => array(
         'title' => 'Are you a Veteran?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'Are you of Hispanic or Latino descent' => array(
+      'Are_you_of_Hispanic_or_Latino_descent' => array(
         'title' => 'Are you of Hispanic or Latino descent',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'Please select the category that best reflects your ethnic background' => array(
+      'Please_select_the_category_that_best_reflects_your_ethnic_background' => array(
         'title' => 'Please select the category that best reflects your ethnic background',
       ),
-      'Religious Preference' => array(
+      'Religious_Preference' => array(
         'title' => 'Religious Preference',
       ),
-      'First Name' => array(
+      'First_Name_2' => array(
         'title' => 'First Name',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 124,
       ),
-      'Last Name' => array(
+      'Last_Name_2' => array(
         'title' => 'Last Name',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 180,
       ),
-      'Phone' => array(
+      'Phone_1' => array(
         'title' => 'Phone',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 125,
       ),
-      'Email' => array(
+      'Email_2' => array(
         'title' => 'Email',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 126,
       ),
-      'Education Level' => array(
+      'Education_Level_1' => array(
         'title' => 'Education Level',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 127,
       ),
-      'Relationship' => array(
+      'Relationship_1' => array(
         'title' => 'Relationship',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 239,
       ),
-      'First Name' => array(
+      'First_Name_3' => array(
         'title' => 'First Name',
+        'same_alias' => TRUE,
+        'alias' => 3,
+        'cid' => 128,
       ),
-      'Last Name' => array(
+      'Last_Name_3' => array(
         'title' => 'Last Name',
+        'same_alias' => TRUE,
+        'alias' => 3,
+        'cid' => 187,
       ),
-      'Phone' => array(
+      'Phone_2' => array(
         'title' => 'Phone',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 129,
       ),
-      'Email' => array(
+      'Email_3' => array(
         'title' => 'Email',
+        'same_alias' => TRUE,
+        'alias' => 3,
+        'cid' => 130,
       ),
-      'Education Level' => array(
+      'Education_Level_2' => array(
         'title' => 'Education Level',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 131,
       ),
-      'Relationship' => array(
+      'Relationship_2' => array(
         'title' => 'Relationship',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 240,
       ),
-      'With which parent do you reside?' => array(
+      'With_which_parent_do_you_reside' => array(
         'title' => 'With which parent do you reside?',
       ),
-      'School_Name' => array(
+      'School_Name_1' => array(
         'title' => 'School Name',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 42,
       ),
-      'School_City_State' => array(
+      'School_City_State_1' => array(
         'title' => 'School City & State',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 43,
       ),
-      'Dates_Attended' => array(
+      'Dates_Attended_1' => array(
         'title' => 'Dates Attended',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 44,
       ),
-      'Graduation_Date' => array(
+      'Graduation_Date_1' => array(
         'title' => 'Graduation Date',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 45,
       ),
-      'Degree_Earned' => array(
+      'Degree_Earned_1' => array(
         'title' => 'Degree(s) Earned',
+        'same_alias' => TRUE,
+        'alias' => 1,
+        'cid' => 46,
       ),
-      'School_Name' => array(
+      'School_Name_2' => array(
         'title' => 'School Name',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 57,
       ),
-      'School_City_State' => array(
+      'School_City_State_2' => array(
         'title' => 'School City & State',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 58,
       ),
-      'Dates_Attended' => array(
+      'Dates_Attended_2' => array(
         'title' => 'Dates Attended',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 59,
       ),
-      'Graduation_Date' => array(
+      'Graduation_Date_2' => array(
         'title' => 'Graduation Date',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 60,
       ),
-      'Degree_Earned' => array(
+      'Degree_Earned_2' => array(
         'title' => 'Degree(s) Earned',
+        'same_alias' => TRUE,
+        'alias' => 2,
+        'cid' => 61,
       ),
-      'Have you ever taken a college level course?' => array(
+      'Have_you_ever_taken_a_college_level_course' => array(
         'title' => 'Have you ever taken a college level course?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'Did you/Do you participate in early college?' => array(
+      'Did_you_Do_you_participate_in_early_college' => array(
         'title' => 'Did you/Do you participate in early college?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'Have you attended another college?' => array(
+      'Have_you_attended_another_college' => array(
         'title' => 'Have you attended another college?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'School_Name' => array(
+      'School_Name_3' => array(
         'title' => 'School Name',
+        'same_alias' => TRUE,
+        'alias' => 3,
+        'cid' => 52,
       ),
-      'School_City_State' => array(
+      'School_City_State_3' => array(
         'title' => 'School City & State',
+        'same_alias' => TRUE,
+        'alias' => 3,
+        'cid' => 53,
       ),
-      'Dates_Attended' => array(
+      'Dates_Attended_3' => array(
         'title' => 'Dates Attended',
+        'same_alias' => TRUE,
+        'alias' => 3,
+        'cid' => 54,
       ),
-      'Graduation_Date' => array(
+      'Graduation_Date_3' => array(
         'title' => 'Graduation Date',
+        'same_alias' => TRUE,
+        'alias' => 3,
+        'cid' => 55,
       ),
-      'Degree_Earned' => array(
+      'Degree_Earned_3' => array(
         'title' => 'Degree(s) Earned',
+        'same_alias' => TRUE,
+        'alias' => 3,
+        'cid' => 56,
       ),
-      'School_Name' => array(
+      'School_Name_4' => array(
         'title' => 'School Name',
+        'same_alias' => TRUE,
+        'alias' => 4,
+        'cid' => 47,
       ),
-      'School_City_State' => array(
+      'School_City_State_4' => array(
         'title' => 'School City & State',
+        'same_alias' => TRUE,
+        'alias' => 4,
+        'cid' => 48,
       ),
-      'Dates_Attended' => array(
+      'Dates_Attended_4' => array(
         'title' => 'Dates Attended',
+        'same_alias' => TRUE,
+        'alias' => 4,
+        'cid' => 49,
       ),
-      'Graduation_Date' => array(
+      'Graduation_Date_4' => array(
         'title' => 'Graduation Date',
+        'same_alias' => TRUE,
+        'alias' => 4,
+        'cid' => 50,
       ),
-      'Degree_Earned' => array(
+      'Degree_Earned_4' => array(
         'title' => 'Degree(s) Earned',
+        'same_alias' => TRUE,
+        'alias' => 4,
+        'cid' => 51,
       ),
       'Unweighted GPA' => array(
         'title' => 'Unweighted GPA',
       ),
-      'I would like to apply Test Optional' => array(
+      'I_would_like_to_apply_Test_Optional' => array(
         'title' => 'I would like to apply Test Optional',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'Answer these questions' => array(
+      'Answer_these_questions' => array(
         'title' => 'Answer these questions',
       ),
-      'Potential Indication' => array(
+      'Potential_Indication' => array(
         'title' => 'Potential Indication',
       ),
-      'Experiential Learning' => array(
+      'Experiential_Learning' => array(
         'title' => 'Experiential Learning',
       ),
-      'Significant Experience' => array(
+      'Significant_Experience' => array(
         'title' => 'Significant Experience',
       ),
-      'Learning Community' => array(
+      'Learning_Community' => array(
         'title' => 'Learning Community',
       ),
-      'Describe Yourself' => array(
+      'Describe_Yourself' => array(
         'title' => 'Describe Yourself',
       ),
-      'Date Taken' => array(
+      'Date_Taken' => array(
         'title' => 'Date Taken (or planned)',
       ),
-      'Critical Reading Score' => array(
+      'Critical_Reading_Score' => array(
         'title' => 'Critical Reading Score',
       ),
-      'Math Score' => array(
+      'Math_Score' => array(
         'title' => 'Math Score',
       ),
-      'Writing Score' => array(
+      'Writing_Score' => array(
         'title' => 'Writing Score',
       ),
-      'ACT Date Taken (or planned)' => array(
+      'ACT_Date_Taken_or_planned' => array(
         'title' => 'ACT Date Taken (or planned)',
       ),
-      'ACT Composite Score' => array(
+      'ACT_Composite_Score' => array(
         'title' => 'ACT Composite Score',
       ),
-      'Have you ever been dismissed for academic or disciplinary reasons from a secondary school or college?' => array(
+      'Have_you_ever_been_dismissed_for_academic_or_disciplinary_reasons_from_a_secondary_school_or_college' => array(
         'title' => 'Have you ever been dismissed for academic or disciplinary reasons from a secondary school or college?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'If yes, please explain' => array(
+      'If_yes_please_explain' => array(
         'title' => 'If yes, please explain',
       ),
-      'Have you ever been convicted of a crime, other than a minor traffic violation?' => array(
+      'Have_you_ever_been_convicted_of_a_crime_other_than_a_minor_traffic_violation' => array(
         'title' => 'Have you ever been convicted of a crime, other than a minor traffic violation?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'If yes, please explain 2' => array(
+      'If_yes_please_explain_2' => array(
         'title' => 'If yes, please explain 2',
       ),
-      'Do you intend to Graduate from Brevard?' => array(
+      'Do_you_intend_to_Graduate_from_Brevard' => array(
         'title' => 'Do you intend to Graduate from Brevard?',
         'columnName' => "IF(wsd.data=1, 'Yes', 'No')",
       ),
-      'What other colleges or universities are you applying to?' => array(
+      'What_other_colleges_or_universities_are_you_applying_to' => array(
         'title' => 'What other colleges or universities are you applying to?',
       ),
-      'Where do you plan to live?' => array(
+      'Where_do_you_plan_to_live' => array(
         'title' => 'Where do you plan to live?',
       ),
-      'How did you hear about Brevard?' => array(
+      'How_you_hear_about_Brevard' => array(
         'title' => 'How did you hear about Brevard?',
       ),
     );
