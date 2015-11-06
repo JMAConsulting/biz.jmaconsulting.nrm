dc03bf253f242af37561a6fc00338d8c86c36fdc
Edsel <edsel.lopez@jmaconsulting.biz>
2015-11-06 23:58:06 +0530

    NRM-41 Created script to run multiple reports via cron

---
+++ api/v3/Yoteup/Jmamailreport.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * YoteUp JMA Mail Report API
+ *
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_yoteup_jmamailreport($params) {
+  $sql = "SELECT parameters FROM civicrm_job WHERE api_action='mail_report' AND run_frequency='Daily' AND is_active=1";
+  $dao = CRM_Core_DAO::executeQuery($sql);
+  $format = array('format' => 'csv', 'version' => 3);
+  while ($dao->fetch()) {
+    $parameters[] = explode("\n", $dao->parameters);
+  }
+  foreach ($parameters as $key => $value) {
+    foreach ($value as $values) {
+      list($parameter, $option) = explode("=", $values);
+      if ($parameter != 'format') {
+        $instances[] = array($parameter => (int)$option);
+      }
+    }
+  }
+  foreach ($instances as $instance) {
+    $instance = array_merge($instance, $format);
+    $result = CRM_Report_Utils_Report::processReport($instance);
+
+    if ($result['is_error'] != 0) {
+      $messages[] = $result['messages'];
+    }
+  }
+  if (!empty($messages)) {
+    return civicrm_api3_create_error($messages);
+  }
+  else {
+    return civicrm_api3_create_success();
+  }
+}
+
