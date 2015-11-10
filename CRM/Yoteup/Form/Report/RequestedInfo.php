bb7ee03b59562084795f9ead4e58b1164c66da96
Pradeep Nayak <pradpnayak@gmail.com>
2015-11-11 05:16:58 +0530

    --changed code to use column title specified in issue during csv import

--- CRM/Yoteup/Form/Report/RequestedInfo.php
+++ CRM/Yoteup/Form/Report/RequestedInfo.php
@@ -37,52 +37,72 @@ class CRM_Yoteup_Form_Report_RequestedInfo extends CRM_Report_Form {
         'title' => 'Submitted Date',
         'ignore_group_concat' => TRUE,
         'columnName' => 'DATE(FROM_UNIXTIME(ws.completed))',
+        'columnTitle' => 'submitted_date',
       ),
       'First_Name' => array(
         'title' => 'First Name',
+        'columnTitle' => 'First_Name',
       ),
       'Middle_Name' => array(
         'title' => 'Middle Name',
+        'columnTitle' => 'Middle_Name',
       ),
       'Last_Name' => array(
         'title' => 'Last Name',
+        'columnTitle' => 'Last_Name',
       ),
       'Name_Suffix' => array(
         'title' => 'Name Suffix',
         'columnName' => 'suffix_alias.label',
+        'columnTitle' => 'name_suffix',
       ),
       'Nickname' => array(
         'title' => 'Nickname',
+        'columnTitle' => 'Nick_name',
       ),
       'Street_Address' => array(
         'title' => 'Street Address',
+        'columnTitle' => 'Street_Address',
       ),
       'Street_Address_Line_2' => array(
         'title' => 'Street Address Line 2',
+        'columnTitle' => 'Address_Line_2',
       ),
       'City' => array(
-        'title' => 'City'
+        'title' => 'City',
+        'columnTitle' => 'City',
+      ),
+      'County' => array(
+        'title' => 'County',
+        'columnTitle' => 'County',
       ),
       'State_Abbr' => array(
         'title' => 'State Abbr',
+        'columnTitle' => 'State',
       ),
       'Zip_Code' => array(
         'title' => 'Zip Code',
+        'columnTitle' => 'Postal_Code',
       ),
       'Phone_Number' => array(
         'title' => 'Phone Number',
+        'columnTitle' => 'Phone',
       ),
       'Email' => array(
         'title' => 'Email',
+        'columnTitle' => 'Email',
       ),
       'High_School_Name' => array(
         'title' => 'High School Name',
+        'columnTitle' => 'School_1',
       ),
       'High_School_Graduation_Date' => array(
         'title' => 'High School Graduation Date',
+        'columnTitle' => 'HS_Grad_Date',
       ),
       'Major_or_Academic_Interests' => array(
         'title' => 'Major or Academic Interests',
+        'columnTitle' => 'Academic_Interest',
         'columnName' => 'academic_alias.label',
       ),
     );
