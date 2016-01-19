4612090056daebf0dc5b2675a504b94fc0970316
Edsel <edsel.lopez@jmaconsulting.biz>
2016-01-19 15:48:55 +0530

    NRM-70 Added constant for cid
    Conflicts:
    	CRM/Nrm/Form/Report/IndividualCounselor.php

--- nrm_constants.php
+++ nrm_constants.php
@@ -6,6 +6,7 @@ define('GRAD_YEAR', 'hs_graddate_153');
 define('MAJOR', 'major_interests_149');
 define('PURLS', 'civicrm_value_nrmpurls_5');
 define('TERRITORY_COUNSELOR', 446);
+define('CID', 19);
 
 $config = CRM_Core_Config::singleton();
 $host = $config->userFrameworkBaseURL;
